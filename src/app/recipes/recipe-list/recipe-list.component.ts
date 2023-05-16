6f086ae28c03a3aac44b7ddde0f33d2028daafec
Sagar Dighe <sagasoftech@gmail.com>
2023-05-15 23:09:13 -0500

    Adding content to the Recipe Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('The test recipe', 'This is just a test Recipe', 
+    'https://upload.wikimedia.org/wikipedia/commons/a/a1/Kollukattai-Vinayagar-Chathurthi-Recipes.jpg')
+  ];
+
 
 }
