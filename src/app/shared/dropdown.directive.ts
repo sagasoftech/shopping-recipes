6d8dd695bf65cccd84a193c0d64fbc1abea79899
Sagar Dighe <sagasoftech@gmail.com>
2023-05-28 14:05:42 -0500

    Closing the Dropdown by clicking Anywhere outside on page

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -7,7 +7,13 @@ export class DropdownDirective{
     
     @HostBinding('class.open') isOpen = false;
 
-    @HostListener('click') toggleOpen(){
-        this.isOpen = !this.isOpen;
+    // @HostListener('click') toggleOpen(){
+    //     this.isOpen = !this.isOpen;
+    // }
+
+    @HostListener('document:click', ['$event']) toggleOpenDoc(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
+    
+    constructor(private elRef: ElementRef) {}
 }
