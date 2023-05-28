c6f3bf9935619802b0143e774ba1b5f287b24e43
Sagar Dighe <sagasoftech@gmail.com>
2023-05-28 00:37:17 -0500

    Passing data with Event and Property binding (combined)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -9,6 +9,10 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent {
 
   @Input('recipe') recipe: Recipe;
+  @Output('selectedRecipe') selectedRecipe = new EventEmitter<void>();
 
+  onRecipeSelected(){
+    this.selectedRecipe.emit();
+  }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,12 +8,16 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
 
+  @Output('recipeWasSelected') recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('The test recipe', 'This is just a test Recipe', 
     'https://upload.wikimedia.org/wikipedia/commons/a/a1/Kollukattai-Vinayagar-Chathurthi-Recipes.jpg'),
-    new Recipe('The test recipe2', 'This is just a test Recipe', 
+    new Recipe('The test recipe2', 'This is just a test Recipe2', 
     'https://upload.wikimedia.org/wikipedia/commons/a/a1/Kollukattai-Vinayagar-Chathurthi-Recipes.jpg')
   ];
 
-
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
