5b7efc117860577e67cc64461013a09ea032fac3
Sagar Dighe <sagasoftech@gmail.com>
2023-05-29 03:27:28 -0500

    Adding Ingredients to the Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService{
 
@@ -7,9 +8,17 @@ export class RecipeService{
 
     private recipes: Recipe[] = [
         new Recipe('The test recipe', 'This is just a test Recipe', 
-        'https://upload.wikimedia.org/wikipedia/commons/a/a1/Kollukattai-Vinayagar-Chathurthi-Recipes.jpg'),
+        'https://upload.wikimedia.org/wikipedia/commons/a/a1/Kollukattai-Vinayagar-Chathurthi-Recipes.jpg',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+        ]),
         new Recipe('The test recipe2', 'This is just a test Recipe2', 
-        'https://upload.wikimedia.org/wikipedia/commons/a/a1/Kollukattai-Vinayagar-Chathurthi-Recipes.jpg')
+        'https://upload.wikimedia.org/wikipedia/commons/a/a1/Kollukattai-Vinayagar-Chathurthi-Recipes.jpg',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+        ])
     ];
 
     getRecipes(){
